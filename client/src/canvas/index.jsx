dd959c15bd4e953902f697dc3a44d3092146ab18
Yaniv <Yanivv77@gmail.com>
2023-09-25 20:03:14 +0300

    Home page

---
+++ client/src/canvas/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+function Canvas() {
+  return (
+    <div>index</div>
+  )
+}
+
+export default Canvas
---
+++ client/src/store/index.js
@@ -0,0 +1,17 @@
+import { proxy } from 'valtio'
+
+const state = proxy(
+    {
+        intro: true,
+        color: '#EFBD48',
+        isLogoTexture : true,
+        isFullTexture : false,
+        logoDecal: './three.js.png',
+        fullDecal: './three.js.png',
+        
+
+
+    }
+)
+
+export default state;
